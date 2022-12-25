d0b668c48e87c5a1a1e621b6846f1dc8e58026d2
Saranyajothy <saranyajothikumar12@gmail.com>
2022-12-25 17:53:00 +0000

    added products app,models and fixtures

--- costume_hire/settings.py
+++ costume_hire/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l+dpi-v_r@2rft4b)v#vg1idh$8vuv^v)_!#fa@#)!^ssf89+n'
+SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
